84592364c87f800cc8c2dffd2a85dedb14ef46ad
Rodrigo Aznar <rodrigoaznar75@gmail.com>
2026-05-23 18:52:42 -0300

    custom driver fixes to cover homework 1 requirements

--- app/drivers/my_driver/my_driver.c
+++ app/drivers/my_driver/my_driver.c
@@ -1,26 +1,56 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
 
-#define DT_DRV_COMPAT my_driver
+#define DT_DRV_COMPAT ram_driver
 
-LOG_MODULE_REGISTER(my_driver, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(sensor_ram_driver, LOG_LEVEL_INF);
 
-static int channel_get_impl (const struct device *dev,
-                             enum sensor_channel chan,
-                             struct sensor_value *val)
-{
-    LOG_INF("** Hello channel get - channel: %d", chan);
+
+struct my_driver_config {
+    struct gpio_dt_spec led;
+};
+
+
+static int my_sample_fetch(const struct device *dev, enum sensor_channel chan) {
+    const struct my_driver_config *cfg = dev->config;
+    LOG_INF(" Turn LED ON *** \n");
+    gpio_pin_set_dt(&cfg->led, 1);
     return 0;
 }
 
+
+static int my_get_channel(const struct device *dev, enum sensor_channel chan, struct sensor_value *val) {
+    const struct my_driver_config *cfg = dev->config;
+    LOG_INF(" Turn LED OFF --- \n");
+    gpio_pin_set_dt(&cfg->led, 0);
+    return 0;
+}
+
+
 static DEVICE_API(sensor, api_my_driver) = {
-    .channel_get = channel_get_impl,
+    .channel_get  = my_get_channel,
+    .sample_fetch = my_sample_fetch,    
 };
 
 static int init(const struct device* dev) {
+    const struct my_driver_config *cfg = dev->config;
+
+    gpio_pin_configure_dt(&cfg->led, GPIO_OUTPUT_ACTIVE);    
+    
     LOG_INF(" -- Device Initialized -- ");
 
     return 0;
 }
 
-DEVICE_DT_INST_DEFINE(0, init, NULL, NULL, NULL, POST_KERNEL, 80, &api_my_driver);
+
+// Automatically instantiate a distinct driver instance for every enabled 
+// node matching this driver's compatible string in the devicetree.
+#define DEV_INST(inst)                                 \
+    static const struct my_driver_config my_driver_config_##inst = { \
+        .led = GPIO_DT_SPEC_INST_GET(inst, gpios),     \
+    };                                                 \
+    DEVICE_DT_INST_DEFINE(inst, init, NULL, NULL,   \
+                          &my_driver_config_##inst, POST_KERNEL, 80, &api_my_driver);
+
+DT_INST_FOREACH_STATUS_OKAY(DEV_INST)                 
--- app/src/main.cpp
+++ app/src/main.cpp
@@ -9,16 +9,18 @@
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(app_led)
 
+#define LECTURE_NR 6
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 namespace {
-    void test() {
+    int test() {
         const struct device* dev = DEVICE_DT_GET(DT_NODELABEL(my_driver0));
 
         if (!device_is_ready(dev)) {
-            return;
+            return -ENODEV;
         }
 
         struct sensor_value val;
@@ -28,30 +30,55 @@ namespace {
         if (ret == 0) {
             // Use sensor_value_to_double(&val) or similar to read the data
         }
-
-
+        return ret;
     }
-
-
 }
 
 
 int main(void)
 {
-    test();
+    #if (LECTURE_NR >= 6)
+    struct sensor_value value;
+    const struct device *mydev = DEVICE_DT_GET(DT_NODELABEL(my_driver0));
+
+    if (!device_is_ready(mydev)) {
+        LOG_ERR("Error: My driver device is not ready!\n");
+        return -ENODEV; 
+    }
+
+    /* JUST FOR BASIC TEST */ test();
+    #endif
+
+
     bool led_state = true;
 
+
+    #if (LECTURE_NR < 6) // lessons previous to custom driver
     if (!gpio_is_ready_dt(&led)) return 0;
 
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    #endif
+
+    LOG_INF("Hello World! %s\n", CONFIG_BOARD_TARGET);
 
     while (1) {
+
+        #if (LECTURE_NR < 6) // lessons previous to custom driver
         if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        #endif
 
         led_state = !led_state;
-        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
 
-        printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+        #if (LECTURE_NR >= 6)
+
+        if (led_state)
+            (void)sensor_sample_fetch(mydev);
+        else
+            (void)sensor_channel_get(mydev, SENSOR_CHAN_AMBIENT_TEMP, &value);
+
+        #endif
+
+        LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
 
         k_msleep(CONFIG_APP_HEART_BEAT_PERIOD_MS);
     }
